e360f8a50e574ef92c2fea32ff79d9f693e5948a
mBialczak <82645568+mBialczak@users.noreply.github.com>
2021-12-12 20:14:28 +0100

    main refactored

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 #include "Game.hpp"
-#include "IntervalTimer.hpp" //NOTE: remove if no longer needed
-#include "SDL.h" //NOTE: remove if no longer needed
 #include "SDLexception.hpp"
 #include <exception>
 #include <iostream>
@@ -14,28 +12,28 @@ int main(int argc, char const* argv[])
   constexpr unsigned c_implemented_levels { 10 };
 
   try {
-
+    // create the main game object
     Game simple_arkanoid(c_screen_height, c_screen_width, c_frames_per_second,
         c_implemented_levels);
 
+    // run the main loop
     simple_arkanoid.Run();
-    // REMOVE
-    std::cout << "program terminated normally" << std::endl;
+
     return 0;
   }
   // handle SDL-related exceptions
   catch (const SDLexception& e) {
     std::cerr << e.Description();
-    return -3;
+    return -1;
   }
   // handle standard iherited exceptions
   catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
-    return -1;
+    return -2;
   }
   // handle unpredicted exceptions
   catch (...) {
     std::cerr << "Unknown exception occured!" << std::endl;
-    return -2;
+    return -3;
   }
 }
