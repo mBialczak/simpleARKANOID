9b1339f24aae5107f24621dbbbb88bae75b5646b
Maciej Białczak <m.bialczak@protonmail.com>
2021-12-30 18:06:01 +0100

    REFACTORING: member vars of SDLexception renamed
    Renamed member variables to meet best coding practices

--- headers/SDLexception.hpp
+++ headers/SDLexception.hpp
@@ -4,7 +4,8 @@
 #include <string>
 
 // custom exception class for handling SDL errors
-class SDLexception {
+class SDLexception
+{
   public:
   // constructor
   SDLexception(const std::string& firstPart, const char* errorCodePart,
@@ -15,14 +16,14 @@ class SDLexception {
 
   private:
   // description part provided by the programmer
-  const std::string _firstPart;
+  const std::string firstPart_;
   // description part provided by SDL-related library
-  const std::string _errorCodePart;
+  const std::string errorCodePart_;
   // source code file in which error occured
-  const std::string _file;
+  const std::string file_;
   // line number of the source code file
-  const int _line;
+  const int line_;
   // separator used for output formating
-  const std::string _separator = "\n";
+  const std::string separator_ = "\n";
 };
 #endif // !SDLexception_HPP
--- src/SDLexception.cpp
+++ src/SDLexception.cpp
@@ -4,18 +4,18 @@
 // constructor
 SDLexception::SDLexception(const std::string& firstPart,
     const char* errorCodePart, const char* file, int line)
-    : _firstPart(firstPart)
-    , _errorCodePart(errorCodePart)
-    , _file(file)
-    , _line(line)
+    : firstPart_(firstPart)
+    , errorCodePart_(errorCodePart)
+    , file_(file)
+    , line_(line)
 {
 }
 
 // returns exception description
 std::string SDLexception::Description() const
 {
-  return std::string("SDL-related exception in file: ") + _file
-      + "line no.: " + std::to_string(_line) + _separator
-      + "PROGRAMMER description: " + _firstPart + _separator
-      + "SDL description: " + _errorCodePart + _separator;
+  return std::string("SDL-related exception in file: ") + file_
+      + "line no.: " + std::to_string(line_) + separator_
+      + "PROGRAMMER description: " + firstPart_ + separator_
+      + "SDL description: " + errorCodePart_ + separator_;
 }
